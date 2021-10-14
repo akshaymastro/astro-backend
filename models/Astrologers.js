8b440a07244571e7100b9a039206a0118b23c7b4
ayushimastro <amrikmastro@gmail.com>
2021-10-14 16:47:07 +0530

    model bracket add

--- models/Astrologers.js
+++ models/Astrologers.js
@@ -1,171 +1,173 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
 const constants = require('../common/constants')
-const ObjectId = mongoose.Types.ObjectId;
+const ObjectId = mongoose.Types.ObjectId
 
-const DocSchema = new Schema({
+const DocSchema = new Schema(
+  {
     userName: {
-    type: String,
-    default: ""
+      type: String,
+      default: ''
     },
     email: {
-        type: String,
-        default: "",
-        index: true
+      type: String,
+      default: '',
+      index: true
     },
     phoneNo: {
-        type: String,
-        default: ""
+      type: String,
+      default: ''
     },
     dialCode: {
-        type: String,
-        default: ""
+      type: String,
+      default: ''
     },
     password: {
-        type: String,
-        default: "",
-        index: true
+      type: String,
+      default: '',
+      index: true
     },
     confirmPassword: {
-        type: String,
-        default: "",
-        index: true
+      type: String,
+      default: '',
+      index: true
     },
     image: {
-        type: String,
-        default: ""
+      type: String,
+      default: ''
     },
     country: {
-        type: String,
-        default: ""
+      type: String,
+      default: ''
     },
     city: {
-        type: String,
-        default: ""
+      type: String,
+      default: ''
     },
     address: {
-        type: String,
-        default: ""
+      type: String,
+      default: ''
     },
     latitude: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0
     },
     longitude: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0
     },
     isProfileSetup: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     isEmailVerified: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     isPhoneVerified: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     isActive: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     isBlocked: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     isDeleted: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     loginCount: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0
     },
     accessToken: {
-        type: String,
-        default: "",
-        index: true
+      type: String,
+      default: '',
+      index: true
     },
     deviceToken: {
-        type: String,
-        default: "",
-        index: true
+      type: String,
+      default: '',
+      index: true
     },
     deviceType: {
-        type: String,
-        default: "",
-        enum: ["", "WEB", "IOS", "ANDROID"]
+      type: String,
+      default: '',
+      enum: ['', 'WEB', 'IOS', 'ANDROID']
     },
     secretCode: {
-        type: String,
-        default: ""
+      type: String,
+      default: ''
     },
     secretExpiry: {
-        type: Date,
-        default: 0
+      type: Date,
+      default: 0
     },
     tempData: {
-        type: Object,
-        default: {}
+      type: Object,
+      default: {}
     },
     location: {
-        type: {
-            type: String,
-            default: "Point"
-        },
-        coordinates: {
-            type: [Number],
-            default: [0, 0]
-        },
-    },{
-        timestamps: true
+      type: {
+        type: String,
+        default: 'Point'
+      },
+      coordinates: {
+        type: [Number],
+        default: [0, 0]
+      }
     }
-);
-
+  },
+  {
+    timestamps: true
+  }
+)
 
 DocSchema.index({
-    location: "2dsphere"
-});
+  location: '2dsphere'
+})
 
 DocSchema.index({
-    dialCode: 1,
-    phoneNo: 1
-}); DocSchema.set("toJSON", {
-    getters: true,
-    virtuals: true
-});
+  dialCode: 1,
+  phoneNo: 1
+})
+DocSchema.set('toJSON', {
+  getters: true,
+  virtuals: true
+})
 
 DocSchema.methods.authenticate = function (password, callback) {
-    const promise = new Promise((resolve, reject) => {
-        if (!password) reject(new Error("MISSING_PASSWORD"));
+  const promise = new Promise((resolve, reject) => {
+    if (!password) reject(new Error('MISSING_PASSWORD'))
 
-        bcrypt.compare(password, this.password, (error, result) => {
-            if (!result) reject(new Error("INVALID_PASSWORD"));
-            resolve(this);
-        });
-    });
+    bcrypt.compare(password, this.password, (error, result) => {
+      if (!result) reject(new Error('INVALID_PASSWORD'))
+      resolve(this)
+    })
+  })
 
-    if (typeof callback !== "function") return promise;
-    promise.then((result) => callback(null, result)).catch((err) => callback(err));
-};
+  if (typeof callback !== 'function') return promise
+  promise.then(result => callback(null, result)).catch(err => callback(err))
+}
 
 DocSchema.methods.setPassword = function (password, callback) {
-    const promise = new Promise((resolve, reject) => {
-        if (!password) reject(new Error("Missing Password"));
+  const promise = new Promise((resolve, reject) => {
+    if (!password) reject(new Error('Missing Password'))
 
-        bcrypt.hash(password, 10, (err, hash) => {
-            if (err) reject(err);
-            this.password = hash;
-            resolve(this);
-        });
-    });
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) reject(err)
+      this.password = hash
+      resolve(this)
+    })
+  })
 
-    if (typeof callback !== "function") return promise;
-    promise.then((result) => callback(null, result)).catch((err) => callback(err));
-};
+  if (typeof callback !== 'function') return promise
+  promise.then(result => callback(null, result)).catch(err => callback(err))
+}
 
-
-        module.exports = mongoose.model("Astrologers", DocSchema);
+module.exports = mongoose.model('Astrologers', DocSchema)
